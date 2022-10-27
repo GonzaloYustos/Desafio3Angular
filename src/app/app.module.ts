d97340f0ef06722936fda21eeb60581717434886
Gonzalo Yustos <mgy.yustos@gmail.com>
2022-10-27 17:28:00 -0300

    incorparción de componentes educacion, expereciencia, hys. además las imagenes logo, perfil, banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { HysComponent } from './components/hys/hys.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BannerComponent,
     AcercaDeComponent,
-    HeaderComponent
+    HeaderComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HysComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
